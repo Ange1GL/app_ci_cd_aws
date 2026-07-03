b99c2efefd689f50ea626efcf60444413f739c5c
Angel Larios <alarios@casystem.com.mx>
2026-07-03 13:30:26 -0600

    corrige el endpoint

--- src/main/java/com/app/controller/OcrController.java
+++ src/main/java/com/app/controller/OcrController.java
@@ -5,6 +5,7 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class OcrController
     }
 
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<@NonNull OcrResponse> create() {
         log.info("Entering OcrController.create()");
         return ResponseEntity.ok(new OcrResponse("Create - OCR"));
